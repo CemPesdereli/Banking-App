e2278b6e5f2a0f4f2fddf6479511e7a969da392b
CemPesdereli <cempesdereli@gmail.com>
2025-07-08 21:07:09 +0300

    rabbitmq added async communication for event driven

--- services/accounts/src/main/java/com/cem/accounts/service/impl/AccountsServiceImpl.java
+++ services/accounts/src/main/java/com/cem/accounts/service/impl/AccountsServiceImpl.java
@@ -3,6 +3,7 @@ package com.cem.accounts.service.impl;
 import com.cem.accounts.constants.AccountsConstants;
 import com.cem.accounts.dto.AccountsCustomerDto;
 import com.cem.accounts.dto.AccountsDto;
+import com.cem.accounts.dto.AccountsMsgDto;
 import com.cem.accounts.dto.CustomerDto;
 import com.cem.accounts.entity.Accounts;
 import com.cem.accounts.entity.Customer;
@@ -14,6 +15,9 @@ import com.cem.accounts.repository.AccountsRepository;
 import com.cem.accounts.repository.CustomerRepository;
 import com.cem.accounts.service.IAccountsService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -24,8 +28,12 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class AccountsServiceImpl implements IAccountsService {
 
+    private static final Logger log = LoggerFactory.getLogger(AccountsServiceImpl.class);
+
     private final AccountsRepository accountsRepository;
     private final CustomerRepository customerRepository;
+
+    private final StreamBridge streamBridge;
 
     /**
      * @param customerDto - CustomerDto Object
@@ -41,9 +49,19 @@ public class AccountsServiceImpl implements IAccountsService {
 
 
         Customer savedCustomer = customerRepository.save(customer);
-        accountsRepository.save(createNewAccount(savedCustomer));
+        Accounts savedAccount = accountsRepository.save(createNewAccount(savedCustomer));
+        sendCommunication(savedAccount, savedCustomer);
 
 
+
+    }
+
+    private void sendCommunication(Accounts account, Customer customer) {
+        var accountsMsgDto = new AccountsMsgDto(account.getAccountNumber(), customer.getName(),
+                customer.getEmail(), customer.getMobileNumber());
+        log.info("Sending Communication request for the details: {}", accountsMsgDto);
+        var result = streamBridge.send("sendCommunication-out-0", accountsMsgDto);
+        log.info("Is the Communication request successfully triggered ? : {}", result);
     }
 
 
